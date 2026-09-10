3739aed241aa0e7f0830d220590ceb3fd1f3b633
Daniel <danielbarbosa27@icloud.com>
2026-09-10 09:48:58 -0300

    Definição de elementos das atividades

--- Makefile
+++ Makefile
@@ -4,6 +4,7 @@ REMOVE = rate_dsob.out scheduler edf_dsob.out
 all:
 	gcc main.c -o scheduler
 	./scheduler rate voo.txt
+	./scheduler edf voo.txt
 
 compile:
 	gcc main.c -o scheduler
--- main.c
+++ main.c
@@ -2,16 +2,37 @@
 #include <string.h>
 #include <stdlib.h>
 
+typedef struct{
+    int tempo;
+    char nome[2];
+    int periodo;
+    int deadline;
+    int burst;
+}Escala;
+
 void escala_rate(){
     FILE *file = fopen("rate_dsob.out", "w");
     printf("Funcional Rate\n");
-    fprintf(file,"Funcional Rate");
+    fprintf(file,"Funcional Ratez\n");
 }
 
 void escala_edf(){
     FILE *file = fopen("edf_dsob.out", "w");
     printf("Funcional EDF\n");
-    fprintf(file,"Funcional EDF");
+    fprintf(file,"Funcional EDF\n");
+}
+
+void converter_val(FILE *file, Escala *t){
+    if(fscanf(file, "%d", &t->tempo) != 1){
+        printf("Erro ao ler valor\n");
+        return;
+    }
+    printf("Tempo total: %d\n", t->tempo);
+
+    while(fscanf(file, "%s %d %d %d", t->nome, &t->periodo, &t->deadline, &t->burst) == 4){
+        printf("%s %d %d %d são os especificos dessa atividade atual\n",t->nome, t->periodo, t->deadline, t->burst);
+    }
+    return;
 }
 
 int main(int argc, char **argv){
@@ -20,11 +41,23 @@ int main(int argc, char **argv){
         return 1;
     }
 
+    FILE *file = fopen("voo.txt","r");
+    if(!file){
+        printf("Falha na Localização do Arquivo\n");
+        return 1;
+    }
+
+    Escala t;
+    converter_val(file, t);
     if(strcmp(argv[1], "rate") == 0){
         escala_rate();
     }
     else if(strcmp(argv[1], "edf") == 0){
         escala_edf();
+    }
+    else{
+        printf("Formato de Escalonamento Incorreto\n");
+        return 1;
     }
 
     return 0;
