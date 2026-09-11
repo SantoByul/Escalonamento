48884a5d55822d058288931e91124cfa2a254308
Daniel <danielbarbosa27@icloud.com>
2026-09-11 08:47:37 -0300

    Escalonamento edf simples

--- Makefile
+++ Makefile
@@ -4,6 +4,7 @@ REMOVE = rate_dsob.out scheduler edf_dsob.out
 all:
 	gcc main.c -o scheduler
 	./scheduler edf voo.txt
+	./scheduler rate voo.txt
 
 compile:
 	gcc main.c -o scheduler
--- main.c
+++ main.c
@@ -130,6 +130,78 @@ void escala_edf(Escala *t){
 
 void escala_rate(Escala *t){
     FILE *file = fopen("rate_dsob.out", "w");
+    if(!file){
+        fprintf(stderr,"Erro Envolvendo Arquivo");
+        return;
+    }
+
+    int restante[TOTAL];
+    int prazo[TOTAL];
+    for(int i = 0; i < qnt_tarefas; i++){
+        restante[i] = 0;
+        prazo[i] = 0;
+    }
+    int atual = -1;
+    int inicio = 0;
+
+    for(int i = 0; i < tempo; i++){
+        for(int j = 0; j < qnt_tarefas; j++){
+            if(i % t[j].periodo == 0){
+                restante[j] = t[j].burst;
+                prazo[j] = i + t[j].deadline;
+            }
+        }
+
+        for(int z = 0; z < qnt_tarefas; z++){
+            if(restante[z] > 0 && i >= prazo[z]){
+                restante[z] = 0;
+                if(atual == z){
+                    fprintf(file, "[%s] for %d units - L\n", t[atual].nome, i - inicio);
+                    atual = -1;
+                    inicio = i;
+                }
+            }
+        }
+
+        int proximo = -1;
+        for(int z = 0; z < qnt_tarefas; z++){
+            if(restante[z] > 0){
+                if(proximo == -1 || t[z].periodo < t[proximo].periodo){
+                    proximo = z;
+                }
+            }
+        }
+
+        if(proximo != atual){
+            if(atual == -1){
+                if(i > inicio){
+                    fprintf(file, "idle for %d units\n", i - inicio);
+                }
+            } else {
+                fprintf(file, "[%s] for %d units - H\n", t[atual].nome, i - inicio);
+            }
+            atual = proximo;
+            inicio = i;
+        }
+
+        if(atual != -1){
+            restante[atual]--;
+            if(restante[atual] == 0){
+                fprintf(file, "[%s] for %d units - F\n", t[atual].nome, i - inicio + 1);
+                atual = -1;
+                inicio = i + 1;
+            }
+        }
+    }
+
+    if(atual == -1){
+        if(tempo > inicio){
+            fprintf(file, "idle for %d units\n", tempo - inicio);
+        }
+    } else {
+        fprintf(file, "[%s] for %d units - H\n", t[atual].nome, tempo - inicio);
+    }
+
     fclose(file);
     return;
 }
